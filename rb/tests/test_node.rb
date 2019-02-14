93adf513c298b239c4d172d9c46968df49c236b3
Adriaan Beiertz <adriaanbd@gmail.com>
2019-02-13 22:24:06 -0500

    Renamed node class to MyNode

--- rb/tests/test_node.rb
+++ rb/tests/test_node.rb
@@ -1,32 +1,32 @@
 require 'minitest/autorun'
-load 'node.rb'
+load 'my_node.rb'
 
 class NodeTest < Minitest::Test
     def test_node_returns_node
-        node = Node.new(1)
-        assert_instance_of Node, node
+        node = MyNode.new(1)
+        assert_instance_of MyNode, node
     end
 
     def test_next_returns_nil
-        node = Node.new(2)
+        node = MyNode.new(2)
         assert_nil node.next_pointer
     end
 
     def test_node_returns_key
-        node = Node.new(3)
+        node = MyNode.new(3)
         assert_equal(3, node.key)
     end
 
     def test_next_pointer
-        node1 = Node.new(4)
-        node2 = Node.new(5)
+        node1 = MyNode.new(4)
+        node2 = MyNode.new(5)
         node1.next_pointer = node2
         assert_equal(node2, node1.next_pointer)
         assert_equal(5, node1.next_pointer.key)
     end
 
     def test_key_assignment
-        node = Node.new(6)
+        node = MyNode.new(6)
         assert_raises(NoMethodError) { node.key = 7 }
     end
 end
