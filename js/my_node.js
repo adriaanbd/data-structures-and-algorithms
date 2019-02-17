66ef76809663993d4e1e73dd5191fb0a1c850af4
Adriaan Beiertz <adriaanbd@gmail.com>
2019-02-17 14:15:50 -0500

    Cleaned up leftover comments

--- js/my_node.js
+++ js/my_node.js
@@ -6,23 +6,3 @@ class MyNode {
 }
 
 module.exports = { MyNode };
-
-// let n = new MyNode(3);
-// console.log(n.key, n.next_pointer);
-
-// let s = new MyStack;
-// console.log(s.head, s.min_stack, s.size);
-
-// s.push(10);
-// console.log(s.size, s.head.key, s.min_stack.key);
-
-// s.push(20);
-// console.log(s.size, s.head.key, s.min_stack.key);
-
-// s.push(5);
-// console.log(s.size, s.head.key, s.min_stack.key);
-
-// console.log(5 === s.pop());
-// console.log(s.size);
-// console.log(s.min_stack.key);
-// console.log(s.head.key);
