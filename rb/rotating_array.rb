3bdd2385b1511c66f5caeb40bd34024c4a6000ac
Adriaan Beiertz <adriaanbd@gmail.com>
2019-03-10 00:42:31 -0500

    Adding prob url

--- rb/rotating_array.rb
+++ rb/rotating_array.rb
@@ -1,10 +1,13 @@
-# Second version:
+# CODILITY
+# ROTATING ARRAY
+# https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+
 def solution(a, k)
     k.times { a.unshift(a.pop) } unless a.all? { |i| i == i[0] } || a.length == k
     a
 end
 
-# First version:
+# Other version:
 
 # def solution(a, k)
 #     return a if a.all? { |i| i == i[0] } || a.length == k
